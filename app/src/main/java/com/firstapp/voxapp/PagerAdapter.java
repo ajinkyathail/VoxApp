2d1624e5e914fdd782679e86bae9f458876cbd24
Ajinkya Thail <ajinkyathail@gmail.com>
2021-09-06 10:50:20 +0530

    VoxApp

---
+++ app/src/main/java/com/firstapp/voxapp/PagerAdapter.java
@@ -0,0 +1,33 @@
+package com.firstapp.voxapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int tabcount;
+    public PagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabcount=behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0:
+                return new chatFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/firstapp/voxapp/userprofile.java
@@ -0,0 +1,30 @@
+package com.firstapp.voxapp;
+
+public class userprofile {
+
+    public String username,userID;
+
+    public userprofile() {
+    }
+
+    public userprofile(String username, String userID) {
+        this.username = username;
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
